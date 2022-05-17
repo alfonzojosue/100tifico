51adb8514d215be92945f698677a308aa8503a55
Josue Alfonzo <16-00436@usb.ve>
2022-05-17 09:30:53 -0400

    Router-2

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,19 @@
 import Home from '../pages/Home';
 import Charanters from '../pages/Characters';
 import Error404 from '../pages/Error404';
-import Headers from '../templates/Header';
+import Header from '../templates/Header';
+
+const routes = {
+    '/': Home,
+    '/:id':Charanters,
+    '/contact': 'Contact',
+}
+
+const router = async () => {
+    const header = null || document.getElementById('header');
+    const content = null || document.getElementById('content');
+
+    header.innerHTML = await Header();
+}
+
+export default router;
