6ac338855824294827adbc56cc18f0c1fd98b6af
Josue Alfonzo <16-00436@usb.ve>
2022-05-17 10:05:09 -0400

    render, route & hash

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,9 @@ import Home from '../pages/Home';
 import Charanters from '../pages/Characters';
 import Error404 from '../pages/Error404';
 import Header from '../templates/Header';
+import getHash from '../utils/getHash';
+import resolveRoutes from '../utils/resolveRoutes';
+
 
 const routes = {
     '/': Home,
@@ -14,6 +17,10 @@ const router = async () => {
     const content = null || document.getElementById('content');
 
     header.innerHTML = await Header();
+    let hash = getHash();
+    let route = await resolveRoutes(hash);
+    let render = routes[route] ? routes[route] : Error404;
+    content.innerHTML = await render();
 }
 
 export default router;
