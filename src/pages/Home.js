b5244d45b46bdc86bac8cd47b0461b4be845af04
Josue Alfonzo <16-00436@usb.ve>
2022-05-16 21:40:51 -0400

    Creacion del template de home

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+    const view = `
+    <div class="Characters"></div>
+        <article class= "Character-item">
+            <a href="#/1/>
+                <img src="image" alt="name">
+                    <h2>Name</h2>
+                </a>
+        </article>
+    `;
+    return view
+}
+
+export default Home;
